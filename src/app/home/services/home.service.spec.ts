318f1c3471d558a7c7750de77a951e1ce1334800
zdndd <dongningdella@gmail.com>
2023-11-20 17:16:17 +0800

    10之前

--- src/app/home/services/home.service.spec.ts
+++ src/app/home/services/home.service.spec.ts
@@ -1,12 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { HomeService } from './home.service';
+import { HomeService } from "./home.service";
 
-describe('HomeService', () => {
+describe("HomeService", () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: HomeService = TestBed.get(HomeService);
+  it("should be created", () => {
+    const service: HomeService = TestBed.inject(HomeService);
     expect(service).toBeTruthy();
   });
 });
